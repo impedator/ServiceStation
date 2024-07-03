3b5f4dda460dfe7571ac2754e58c5f5bca291ab5
impedator <krzysztof.strug@gmail.com>
2024-07-04 00:30:20 +0200

    add client in table on main page

--- clients/urls.py
+++ clients/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.client_list, name='client_list'),
     path('add/', views.add_client, name='add_client'),
+    path('add/ajax/', views.add_client_ajax, name='add_client_ajax'),
+    path('delete/<int:client_id>/', views.delete_client_ajax, name='delete_client_ajax'),
     path('<int:pk>/', views.client_detail, name='client_detail'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,7 @@
+import json
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Client
 from .forms import ClientForm
 
@@ -19,3 +22,26 @@ def add_client(request):
     else:
         form = ClientForm()
     return render(request, 'clients/add_client.html', {'form': form})
+
+@csrf_exempt
+def add_client_ajax(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        form = ClientForm(data)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    return JsonResponse({'success': False, 'error': 'Invalid request method'})
+
+@csrf_exempt
+def delete_client_ajax(request, client_id):
+    if request.method == 'DELETE':
+        try:
+            client = Client.objects.get(id=client_id)
+            client.delete()
+            return JsonResponse({'success': True})
+        except Client.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'Client not found'})
+    return JsonResponse({'success': False, 'error': 'Invalid request method'})
